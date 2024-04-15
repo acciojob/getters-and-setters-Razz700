d89a870f3deb6b36bec6b2a6a79fa0bce4e3fa55
Razz700 <160166041+Razz700@users.noreply.github.com>
2024-04-15 22:44:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
 //complete this code
-class Person {}
+let n,a;
+class Person {
+	constructor(name,age){
+		this.n=name;
+		this.a=age;
+	}
+		get name(){return this.n;}
+	set age(age){
+	    this.a=age;
+	}
+}
 
-class Student extends Person {}
+class Student extends Person {
+	constructor(name,age){
+        super(name,age);}
+    study(){
+       return console.log(this.name,"is studying");
+    }
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	constructor(name,age){
+        super(name,age);}
+        teach(){
+           return console.log(this.name,"is teaching");
+        }
+}
 
 // Do not change the code below this line
 window.Person = Person;
